73983afc1d6067bb79ad9c96622397e8f378e895
James Cuadra <62761473+James-Cuadra@users.noreply.github.com>
2023-12-05 14:10:45 +0000

    error handling

--- src/pages/WeekThree.tsx
+++ src/pages/WeekThree.tsx
@@ -3,41 +3,42 @@ import { Container, Grid, Card, Code, TextInput, Button } from '@mantine/core';
 
 const WeekThree: React.FC = () => {
   // Task 1
-  const [task1Result, setTask1Result] = useState<number>();
-  const [task2Result, setTask2Result] = useState<number>();
-  const [task3Result, setTask3Result] = useState<number>();
-  const [task3ResultSubArr, setTask3ResultSubArr] = useState<number[]>([]);
-  const [task4Result, setTask4Result] = useState<string>('');
-  const [task5Result, setTask5Result] = useState<string>('');
+  const [task1Result, setTask1Result] = useState<any>();
+  const [task2Result, setTask2Result] = useState<any>();
+  const [task3Result, setTask3Result] = useState<any>();
+  const [task3ResultSubArr, setTask3ResultSubArr] = useState<any[]>([]);
+  const [task4Result, setTask4Result] = useState<any>('');
+  const [task5Result, setTask5Result] = useState<any>('');
 
   // Task 2
-  const [maximumArray, setMaximumArray] = useState<number[]>([])
-  const [minimumArray, setMinimumArray] = useState<number[]>([])
-  const [twoDArray, setTwoDArray] = useState<string>('')
+  const [maximumArray, setMaximumArray] = useState<number[]>([]);
+  const [minimumArray, setMinimumArray] = useState<number[]>([]);
+  const [twoDArray, setTwoDArray] = useState<string>('');
 
   const maximumProduct = () => {
     // Sort the array in ascending order
-    maximumArray.sort((a:any, b:any) => a - b);
+    maximumArray.sort((a: any, b: any) => a - b);
 
     // Calculate the product of the three largest numbers
     const n = maximumArray.length;
-    const option1 = maximumArray[n - 1] * maximumArray[n - 2] * maximumArray[n - 3];
+    const option1 =
+      maximumArray[n - 1] * maximumArray[n - 2] * maximumArray[n - 3];
 
     // Calculate the product of the two smallest numbers (which could be negative) and the largest number
     const option2 = maximumArray[0] * maximumArray[1] * maximumArray[n - 1];
 
     // Return the maximum of the two options
-    setTask1Result(Math.max(option1, option2))
+    setTask1Result(Math.max(option1, option2));
     console.log(Math.max(option1, option2));
-    
+
     return Math.max(option1, option2);
-  }
+  };
 
   const minimumProduct = () => {
     // Sort the array in ascending order
-    
+
     minimumArray.sort((a: any, b: any) => a - b);
-    
+
     // Calculate the product of the three smallest numbers
     const n = minimumArray.length;
     const option1 = minimumArray[0] * minimumArray[1] * minimumArray[2];
@@ -46,46 +47,48 @@ const WeekThree: React.FC = () => {
     const option2 = minimumArray[0] * minimumArray[1] * minimumArray[n - 1];
 
     // Return the minimum of the two options
-    setTask2Result(Math.min(option1, option2)); 
+    setTask2Result(Math.min(option1, option2));
     return Math.min(option1, option2);
   };
 
-  const handleChangeTask1 = (e:any) => {
+  const handleChangeTask1 = (e: any) => {
     const inputArray = e.target.value.slice(1, -1).split(',').map(Number);
     setMaximumArray(inputArray);
-    console.log({maximumArray});
+    console.log({ maximumArray });
   };
 
-  const handleChangeTask2 = (e:any) => {
+  const handleChangeTask2 = (e: any) => {
     const inputArray = e.target.value.slice(1, -1).split(',').map(Number);
     setMinimumArray(inputArray);
-    console.log({minimumArray});
+    console.log({ minimumArray });
   };
 
-  const handleChangeTask3 =  () => {
-    let arr = JSON.parse(twoDArray);
-    console.log(arr);
-    
-    let largestSubArray: number[] =  [];
-    let largestProduct = -Infinity;
-    let output = arr.map((sub_arr: number[]) => {
-      console.log(sub_arr);
-      let product = sub_arr.reduce((acc, num) => acc * num, 1);
+  const handleChangeTask3 = () => {
+    try {
+      let arr = JSON.parse(twoDArray);
+      console.log(arr);
 
-  // Check if the current sub-array has a larger product than the previously found largest product
-  if (product > largestProduct) {
-    largestProduct = product;
-    largestSubArray = sub_arr.slice(); // Make a copy of the current sub-array
-  }
-  console.log("Product of the Largest Sub-Array:", largestProduct);
-  console.log("The largest sub array:", largestSubArray);
-  
-  setTask3Result(largestProduct)
-  setTask3ResultSubArr(largestSubArray)
-    });
-    
+      let largestSubArray: number[] = [];
+      let largestProduct = -Infinity;
+      let output = arr.map((sub_arr: number[]) => {
+        console.log(sub_arr);
+        let product = sub_arr.reduce((acc, num) => acc * num, 1);
+
+        // Check if the current sub-array has a larger product than the previously found largest product
+        if (product > largestProduct) {
+          largestProduct = product;
+          largestSubArray = sub_arr.slice(); // Make a copy of the current sub-array
+        }
+        console.log('Product of the Largest Sub-Array:', largestProduct);
+        console.log('The largest sub array:', largestSubArray);
+
+        setTask3Result(largestProduct);
+        setTask3ResultSubArr(largestSubArray);
+      });
+    } catch (error: any) {
+      setTask3Result('Error parsing JSON: ' + error.message);
+    }
   };
-
 
   const t1Code = '';
   const t2Code = '';
@@ -93,9 +96,8 @@ const WeekThree: React.FC = () => {
   const t4Code = '';
   const t5Code = '';
 
-  console.log({twoDArray})
-  console.log({task3ResultSubArr});
-  
+  console.log({ twoDArray });
+  console.log({ task3ResultSubArr });
 
   return (
     <Container size="sm">
@@ -108,18 +110,13 @@ const WeekThree: React.FC = () => {
             <p>Find Max 3-Digit Product</p>
             <Code>{t1Code}</Code>
             <TextInput
-                label="Enter an array"
-                id="b"
-                onChange={handleChangeTask1}
-              />
-            <Button
-                onClick={
-                 maximumProduct
-                }
-                mt="sm"
-                mr="lg">
-                Maximum number
-              </Button>
+              label="Enter an array"
+              id="b"
+              onChange={handleChangeTask1}
+            />
+            <Button onClick={maximumProduct} mt="sm" mr="lg">
+              Maximum number
+            </Button>
             <p className="mt-3">{task1Result}</p>
           </Card>
         </Grid.Col>
@@ -129,18 +126,13 @@ const WeekThree: React.FC = () => {
             <p>Minimum Number</p>
             <Code>{t2Code}</Code>
             <TextInput
-                label="Enter an array"
-                id="b"
-                onChange={handleChangeTask2}
-              />
-            <Button
-                onClick={
-                  minimumProduct
-                }
-                mt="sm"
-                mr="lg">
-                Maximum number
-              </Button>
+              label="Enter an array"
+              id="b"
+              onChange={handleChangeTask2}
+            />
+            <Button onClick={minimumProduct} mt="sm" mr="lg">
+              Maximum number
+            </Button>
             <p className="mt-3">{task2Result}</p>
           </Card>
         </Grid.Col>
@@ -150,16 +142,15 @@ const WeekThree: React.FC = () => {
             <p>Largest Sub Array</p>
             <Code>{t3Code}</Code>
             <TextInput
-                label="Enter an array"
-                onChange={(e:any) => setTwoDArray(e.target.value)}
-              />
-            <Button
-              onClick={handleChangeTask3}
-                mt="sm"
-                mr="lg">
-                Maximum number of subarray
-              </Button>
-              <p className="mt-3">Largest Sub-Array: {task3ResultSubArr.join(', ')}</p>
+              label="Enter an array"
+              onChange={(e: any) => setTwoDArray(e.target.value)}
+            />
+            <Button onClick={handleChangeTask3} mt="sm" mr="lg">
+              Maximum number of subarray
+            </Button>
+            <p className="mt-3">
+              Largest Sub-Array: {task3ResultSubArr.join(', ')}
+            </p>
             <p className="mt-3">Largest Product: {task3Result}</p>
           </Card>
         </Grid.Col>
