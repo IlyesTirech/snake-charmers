2b5eff5d3ca19235633be06fd99d11109e368a36
Ilyes Tirech <72514875+IlyesTirech@users.noreply.github.com>
2024-02-26 12:13:17 +0000

    random gens

--- src/pages/WeekOne.tsx
+++ src/pages/WeekOne.tsx
@@ -3,6 +3,7 @@ import { Container, Grid, Card, TextInput, Button, Checkbox, Slider } from '@man
 
 const WeekOne: React.FC = () => {
 
+    const [password, setPassword] = useState<string>('')
     const [passwordLength, setPasswordLength] = useState<number>(0)
     const [isUppercase, setIsUppercase] = useState<boolean>(false);
     const [isLowercase, setIsLowercase] = useState<boolean>(false);
@@ -15,9 +16,44 @@ const WeekOne: React.FC = () => {
 
 
     const generatePassword = () => {
-      // rand string generator given a length
-        // 
-    }
+
+      const random = (min,max) => {
+        // Function to generate a random number between min and max (inclusive)
+        // ...
+      };
+      
+      const randomLower = () => {
+        // Function to generate a random lowercase letter
+        // ...
+      };
+      
+      const randomUpper = () => {
+        // Function to generate a random uppercase letter
+        // ...
+      };
+      
+      const randomSymbol = () => {
+        // Function to generate a random symbol
+        // ...
+      };
+  
+      let password = "";
+      for (let i = 0; i < passwordLength; i++) {
+        let choice = random(0, 3);
+        if (isLowercase && choice === 0) {
+          password += randomLower();
+        } else if (isUppercase && choice === 1) {
+          password += randomUpper();
+        } else if (isSymbol && choice === 2) {
+          password += randomSymbol();
+        } else if (isNumber && choice === 3) {
+          password += random(0, 9);
+        } else {
+          i--;
+        }
+      }
+      setPassword(password);
+    };
     
 
     return (
